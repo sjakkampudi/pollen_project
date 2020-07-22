d9813d7ffa2fe93134201d106161586610247a64
Matthew Larkins <larkins.07@gmail.com>
2020-07-21 22:12:35 -0700

    pollen_w_noising now randomly adds noise, rotates, or both to specified number of images per class

--- anu/pollen_w_noising.py
+++ anu/pollen_w_noising.py
@@ -8,12 +8,17 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, models, optimizers
-from tensorflow.random import set_seed
 from sklearn.model_selection import train_test_split
 import IPython
 import kerastuner as kt
 
 from skimage.util import random_noise
+from pathlib import Path
+import cv2
+
+NUM_IMAGES_PER_CLASS = 20000
+NUM_EPOCHS = 10
+
 
 
 
@@ -22,32 +27,160 @@ os.environ['PYTHONHASHSEED']=str(seed_value)
 
 random.seed(seed_value)
 np.random.seed(seed_value)
-set_seed(seed_value)
+tf.random.set_seed(seed_value)
 
 
 print("tensorflow version is:", tf.__version__)
 
-train_path1 = open('/home/agtrivedi/repos/pollen_project/segged_data/segmented_pollen/1/paths.txt').read().splitlines()
-train_path2 = open('/home/agtrivedi/repos/pollen_project/segged_data/segmented_pollen/2/paths.txt').read().splitlines()
-train_path3 = open('/home/agtrivedi/repos/pollen_project/segged_data/segmented_pollen/3/paths.txt').read().splitlines()
-train_path4 = open('/home/agtrivedi/repos/pollen_project/segged_data/segmented_pollen/4/paths.txt').read().splitlines()
+train_path1 = open('../segged_data/segmented_pollen/1/paths.txt').read().splitlines()
+train_path2 = open('../segged_data/segmented_pollen/2/paths.txt').read().splitlines()
+train_path3 = open('../segged_data/segmented_pollen/3/paths.txt').read().splitlines()
+train_path4 = open('../segged_data/segmented_pollen/4/paths.txt').read().splitlines()
 
 print("Total class 1 images:", len(train_path1))
 print("Total class 2 images:", len(train_path2))
 print("Total class 3 images:", len(train_path3))
 print("Total class 4 images:", len(train_path4))
 
+CLASS_1_PREFIX = '../segged_data/seg_pollen_rgb/1/'
+CLASS_2_PREFIX = '../segged_data/seg_pollen_rgb/2/'
+CLASS_3_PREFIX = '../segged_data/seg_pollen_rgb/3/'
+CLASS_4_PREFIX = '../segged_data/seg_pollen_rgb/4/'
+
+train_images_1 = []
+train_images_2 = []
+train_images_3 = []
+train_images_4 = []
+
+
+#*********** READ IN SOME DATA ************#
+for train_path in train_path1:
+    image_path = CLASS_1_PREFIX + Path(train_path).stem + Path(train_path).suffix
+    train_images_1.append(cv2.imread(image_path))
+for train_path in train_path2:
+    image_path = CLASS_2_PREFIX + Path(train_path).stem + Path(train_path).suffix
+    train_images_2.append(cv2.imread(image_path))
+for train_path in train_path3:
+    image_path = CLASS_3_PREFIX + Path(train_path).stem + Path(train_path).suffix
+    train_images_3.append(cv2.imread(image_path))
+for train_path in train_path4:
+    image_path = CLASS_4_PREFIX + Path(train_path).stem + Path(train_path).suffix
+    train_images_4.append(cv2.imread(image_path))
+    
+    
+print("----- AUGMENTING DATA -----")
+########### Augmentation for class 1 ###############
+i = 0
+rotations = [cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE]
+noise = ['gaussian', 'poisson', 's&p', 'speckle']
+original_data_length = len(train_images_1)
+#*********** AUGMENT SOME DATA ************#
+while len(train_images_1) < NUM_IMAGES_PER_CLASS:
+    img = train_images_1[i]
+    i = (i + 1) % original_data_length # cycle through original data only
+    random_noise_type_index = random.randint(0, 3)
+    random_rotation = random.randint(0, 2)
+    noise_rotate_or_both = random.randint(0, 2)
+    # only add noise
+    if noise_rotate_or_both == 0:
+        img = random_noise(img, noise[random_noise_type_index])
+    # only rotate
+    elif noise_rotate_or_both == 1:
+        img = cv2.rotate(img, rotations[random_rotation])
+    # add noise and rotate
+    else:
+        img = random_noise(img, noise[random_noise_type_index])
+        img = cv2.rotate(img, rotations[random_rotation])
+    train_images_1.append(img)
+
+print('New total class 1 images: ' + str(len(train_images_1)))
+
+########### Augmentation for class 2 ###############
+i = 0
+rotations = [cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE]
+noise = ['gaussian', 'poisson', 's&p', 'speckle']
+original_data_length = len(train_images_2)
+#*********** AUGMENT SOME DATA ************#
+while len(train_images_2) < NUM_IMAGES_PER_CLASS:
+    img = train_images_2[i]
+    i = (i + 1) % original_data_length # cycle through original data only
+    random_noise_type_index = random.randint(0, 3)
+    random_rotation = random.randint(0, 2)
+    noise_rotate_or_both = random.randint(0, 2)
+    # only add noise
+    if noise_rotate_or_both == 0:
+        img = random_noise(img, noise[random_noise_type_index])
+    # only rotate
+    elif noise_rotate_or_both == 1:
+        img = cv2.rotate(img, rotations[random_rotation])
+    # add noise and rotate
+    else:
+        img = random_noise(img, noise[random_noise_type_index])
+        img = cv2.rotate(img, rotations[random_rotation])
+    train_images_2.append(img)
+
+print('New total class 2 images: ' + str(len(train_images_2)))
+
+########### Augmentation for class 3 ###############
+i = 0
+rotations = [cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE]
+noise = ['gaussian', 'poisson', 's&p', 'speckle']
+original_data_length = len(train_images_3)
+#*********** AUGMENT SOME DATA ************#
+while len(train_images_3) < NUM_IMAGES_PER_CLASS:
+    img = train_images_3[i]
+    i = (i + 1) % original_data_length # cycle through original data only
+    random_noise_type_index = random.randint(0, 3)
+    random_rotation = random.randint(0, 2)
+    noise_rotate_or_both = random.randint(0, 2)
+    # only add noise
+    if noise_rotate_or_both == 0:
+        img = random_noise(img, noise[random_noise_type_index])
+    # only rotate
+    elif noise_rotate_or_both == 1:
+        img = cv2.rotate(img, rotations[random_rotation])
+    # add noise and rotate
+    else:
+        img = random_noise(img, noise[random_noise_type_index])
+        img = cv2.rotate(img, rotations[random_rotation])
+    train_images_3.append(img)
+
+print('New total class 3 images: ' + str(len(train_images_3)))
+
+########### Augmentation for class 4 ###############
+i = 0
+rotations = [cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE]
+noise = ['gaussian', 'poisson', 's&p', 'speckle']
+original_data_length = len(train_images_4)
+#*********** AUGMENT SOME DATA ************#
+while len(train_images_4) < NUM_IMAGES_PER_CLASS:
+    img = train_images_4[i]
+    i = (i + 1) % original_data_length # cycle through original data only
+    random_noise_type_index = random.randint(0, 3)
+    random_rotation = random.randint(0, 2)
+    noise_rotate_or_both = random.randint(0, 2)
+    # only add noise
+    if noise_rotate_or_both == 0:
+        img = random_noise(img, noise[random_noise_type_index])
+    # only rotate
+    elif noise_rotate_or_both == 1:
+        img = cv2.rotate(img, rotations[random_rotation])
+    # add noise and rotate
+    else:
+        img = random_noise(img, noise[random_noise_type_index])
+        img = cv2.rotate(img, rotations[random_rotation])
+    train_images_4.append(img)
+
+print('New total class 4 images: ' + str(len(train_images_4)))
+
 train_images = []
 train_labels = []
 
-# first element for padding, classes begin at 1
-label_counts = [len(train_path1), len(train_path2), len(train_path3), len(train_path4)] 
-
+label_counts = [len(train_images_1), len(train_images_2),len(train_images_3), len(train_images_4)]
 total_labels = label_counts[0] + label_counts[1] + label_counts[2] + label_counts[3]
 
-type1, type2, type3, type4 = 0, 0, 0, 0
-
-print("----- AUMGENTING DATA -----")
+############# Shuffling the data ##############
+print("----- SHUFFLING DATA -----")
 for i in range(total_labels):
     # Get random label that is still available
     while True:
@@ -60,120 +193,29 @@ for i in range(total_labels):
     train_labels.append([random_label - 1])
 
     if random_label == 1:
-        path = train_path1.pop(len(train_path1) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image_45 = image.rotate(45)
-        #image_90 = image.rotate(90)
-        #image_90 = image.filter(ImageFilter.GaussianBlur(1))
-        
-        ### Get Noisy!
-        image_90 = np.array(image)
-        image_90 = random_noise(cv_image, mode='gaussian', var=0.025)
-        ### Done Getting Noisy~
-        
-        image = np.asarray(image, dtype=np.float64)
-        image_45 = np.asarray(image_45, dtype=np.float64)
-        image_90 = np.asarray(image_90, dtype=np.float64)
-        train_images.append(image)
-        train_images.append(image_45)
-        train_images.append(image_90)
-        label = [0]
-        train_labels.append(label)
-        train_labels.append(label)
-        type1 += 3
+        img = train_images_1.pop(len(train_images_1) -1) # get the image at the end of the list
+        img = np.asarray(img, dtype=np.float64)
+        train_images.append(img)
     elif random_label == 2:
-        path = train_path2.pop(len(train_path2) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image_45 = image.rotate(45)
-        image_90 = image.rotate(90)
-        #image_90 = image.filter(ImageFilter.GaussianBlur(1))
-        image_60 = image.rotate(60)
-        image_15 = image.rotate(15)
-        image = np.asarray(image, dtype=np.float64)
-        image_45 = np.asarray(image_45, dtype=np.float64)
-        image_90 = np.asarray(image_90, dtype=np.float64)
-        image_60 = np.asarray(image_60, dtype=np.float64)
-        image_15 = np.asarray(image_15, dtype=np.float64)
-        train_images.append(image)
-        train_images.append(image_45)
-        train_images.append(image_90)
-        train_images.append(image_60)
-        train_images.append(image_15)
-        label = [1]
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        type2 += 5
+        img = train_images_2.pop(len(train_images_2) -1) # get the image at the end of the list
+        img = np.asarray(img, dtype=np.float64)
+        train_images.append(img)
     elif random_label == 3:
-        path = train_path3.pop(len(train_path3) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image = np.asarray(image, dtype=np.float64)
-        train_images.append(image)
-        type3 += 1
+        img = train_images_3.pop(len(train_images_3) -1) # get the image at the end of the list
+        img = np.asarray(img, dtype=np.float64)
+        train_images.append(img)
     elif random_label == 4:
-        path = train_path4.pop(len(train_path4) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image_45 = image.rotate(45)
-        #image_90 = image.filter(ImageFilter.GaussianBlur(1))
-        image_90 = image.rotate(90)
-        image_60 = image.rotate(60)
-        image_15 = image.rotate(15)
-        image = np.asarray(image, dtype=np.float64)
-        image_45 = np.asarray(image_45, dtype=np.float64)
-        image_90 = np.asarray(image_90, dtype=np.float64)
-        image_60 = np.asarray(image_60, dtype=np.float64)
-        image_15 = np.asarray(image_15, dtype=np.float64)
-        train_images.append(image)
-        train_images.append(image_45)
-        train_images.append(image_90)
-        train_images.append(image_60)
-        train_images.append(image_15)
-        label = [3]
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        type4 += 5
+        img = train_images_4.pop(len(train_images_4) -1) # get the image at the end of the list
+        img = np.asarray(img, dtype=np.float64)
+        train_images.append(img)
     else:
         print("Issue...")
 
 train_images = np.asarray(train_images)
 train_labels = np.asarray(train_labels)
 
-#train_images = np.reshape(train_images.shape[0], train_images.shape[1], train_images.shape[2], 1)
-train_images_new = np.ones((train_images.shape[0], train_images.shape[1], train_images.shape[2], 1))
-
-for i in range(len(train_images)):
-    image = train_images[i]
-    #print(image.shape,  train_images.shape)
-    image = image.reshape(image.shape[0],image.shape[1],1) # <-- this makes it a rank-3 image
-    #print(image.shape)
-    train_images_new[i] = image
-
-print(train_images_new.shape)
-train_images = train_images_new
-
-print("New image count:", train_images.shape[0])
-
-train_1_count, train_2_count, train_3_count, train_4_count = 0, 0, 0, 0
-
-for i in range(len(train_labels)):
-    if train_labels[i,0] == 0:
-        train_1_count += 1
-    elif train_labels[i,0] == 1:
-        train_2_count += 1
-    elif train_labels[i,0] == 2:
-        train_3_count += 1
-    elif train_labels[i,0] == 3:
-        train_4_count += 1
-
-total = train_1_count + train_2_count + train_3_count + train_4_count
-
-print("Out of", total, "images, there are", train_1_count, "in class 1,", train_2_count, \
-      "in class 2,", train_3_count, "in class 3,", "and", train_4_count, "in class 4")
-
-
+print("New total training images: " + str(len(train_images)))
+print("trains_images.shape: " + str(train_images.shape))
 
 secret_images, test_images, secret_labels, test_labels = train_test_split(train_images,
                                                                        train_labels,
@@ -220,11 +262,6 @@ total = test_1_count + test_2_count + test_3_count + test_4_count
 print("Out of", total, "testing images, there are", test_1_count, "in class 1,", test_2_count, \
       "in class 2,", test_3_count, "in class 3,", "and", test_4_count, "in class 4\n\n")
 
-train_images = train_images / 255
-test_images = test_images / 255
-
-print(train_images[1,:,:].shape)
-
 ''' # comment this out if you want to use the keras tuner
 
 def model_builder(hp):
@@ -261,16 +298,16 @@ tuner.search(train_images,train_labels, epochs = 10, validation_data = (test_ima
 # Get the optimal hyperparameters
 best_hps = tuner.get_best_hyperparameters(num_trials = 1)[0]
 
-print("The hyperparameter search is complete. The optimal number of units in the first densely-connected layer is {best_hps.get('units')} and the optimal learning rate for the optimizer is {best_hps.get('learning_rate')}.")        
+print("The hyperparameter search is complete. The optimal number of units in the first densely-connected layer is {best_hps.get('units')} and the optimal learning rate for the optimizer is {best_hps.get('learning_rate')}.")
 
 model = tuner.hypermodel.build(best_hps)
 model.fit(train_images, train_labels, epochs = 10, validation_data = (test_images, test_labels))
 
 '''
 
-model = models.Sequential() 
+model = models.Sequential()
 
-model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(84, 84, 1)))
+model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(84, 84, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 
 model.add(layers.Conv2D(16, (3, 3), activation='relu'))
@@ -282,7 +319,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 
-model.add(layers.Flatten()) 
+model.add(layers.Flatten())
 
 model.add(layers.Dense(168, activation='relu'))
 model.add(layers.Dense(4)) # final Dense layer has 4 neurons representing the 4 classes
@@ -334,6 +371,9 @@ print("Class 3 accuracy:", int(100*(1-incorrect_3/len(incorrect_labels))), "%")
 print("Class 4 accuracy:", int(100*(1-incorrect_4/len(incorrect_labels))), "%")
 
 # ''' # comment this quotes set out if you want to use the keras manually added layers
+
+
+
 
 
 
