423816d9c82dcba801f6877e1e55345c95f017f9
Srikanth Jakkampudi <srikanth@srikanths-mbp.lan>
2021-04-19 20:57:50 -0400

    created demo branch

--- anu/pollen.py
+++ anu/pollen.py
@@ -1,32 +1,47 @@
 import os
 import random
-from PIL import Image, ImageFilter
+from PIL import Image
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import models, layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras import datasets, models, optimizers
-from tensorflow.random import set_seed
 from sklearn.model_selection import train_test_split
-import IPython
-import kerastuner as kt
+import argparse
 
-seed_value = 1
-os.environ['PYTHONHASHSEED']=str(seed_value)
+from configobj import ConfigObj
 
-random.seed(seed_value)
-np.random.seed(seed_value)
-set_seed(seed_value)
+def parse_args():
+    parser = argparse.ArgumentParser(description="pollen classifier")
+
+    parser.add_argument("config_path", type=str)
+
+    args = parser.parse_args()
+    return args
+
+args = parse_args()
+
+cfg = ConfigObj(args.config_path)
+
+os.environ['PYTHONHASHSEED']=str(cfg["seed_value"])
+
+random.seed(cfg["seed_value"])
+np.random.seed(cfg["seed_value"])
+tf.random.set_seed(cfg["seed_value"])
 
 
 print("tensorflow version is:", tf.__version__)
 
-train_path1 = open('/home/agtrivedi/repos/pollen_project/anu/train/images/1/train_OBJ/paths.txt').read().splitlines()
-train_path2 = open('/home/agtrivedi/repos/pollen_project/anu/train/images/2/train_OBJ/paths.txt').read().splitlines()
-train_path3 = open('/home/agtrivedi/repos/pollen_project/anu/train/images/3/train_OBJ/paths.txt').read().splitlines()
-train_path4 = open('/home/agtrivedi/repos/pollen_project/anu/train/images/4/train_OBJ/paths.txt').read().splitlines()
+train_path1 = open('train/images/1/train_OBJ/paths.txt').read().splitlines()
+train_path2 = open('train/images/2/train_OBJ/paths.txt').read().splitlines()
+train_path3 = open('train/images/3/train_OBJ/paths.txt').read().splitlines()
+train_path4 = open('train/images/4/train_OBJ/paths.txt').read().splitlines()
+
+if cfg["balance_classes"]:
+    train_path1 = train_path1[:cfg["class_size"]]
+    train_path2 = train_path2[:cfg["class_size"]]
+    train_path3 = train_path3[:cfg["class_size"]]
+    train_path4 = train_path4[:cfg["class_size"]]
 
 print("Total class 1 images:", len(train_path1))
 print("Total class 2 images:", len(train_path2))
@@ -43,312 +58,51 @@ total_labels = label_counts[0] + label_counts[1] + label_counts[2] + label_count
 
 type1, type2, type3, type4 = 0, 0, 0, 0
 counter1, counter2, counter3, counter4 = 0, 0, 0, 0
-print("----- AUMGENTING DATA -----")
-for i in range(total_labels):
-    # Get random label that is still available
-    while True:
-        random_label = random.randint(1, 4)
-        if (label_counts[random_label-1] > 0):
-            label_counts[random_label-1] = label_counts[random_label-1] - 1 # decrement the label count
-            break
-    
-    # append the label to the label list and add the corresponding image to the image list
-    train_labels.append([random_label - 1])
 
-    if random_label == 1:
-        path = train_path1.pop(len(train_path1) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image_45 = image.rotate(45)
-        image_90 = image.rotate(90)
-        image_blur = image.filter(ImageFilter.GaussianBlur(1))
-        image_60 = image.rotate(60)
-        image_60 = np.asarray(image_60, dtype=np.float64)
-        image_blur = np.asarray(image_blur, dtype=np.float64)
-        train_images.append(image_blur)
-        train_images.append(image_60)
-        label = [0]
-        train_labels.append(label)
-        train_labels.append(label)
-        type1 += 2
-        if counter1 % 2 == 0: 
-            image_80 = image.rotate(80)
-            image_80 = np.asarray(image_80, dtype=np.float64)
-            train_images.append(image_80)
-            label = [0]
-            train_labels.append(label)
-            type1 += 1
+train_images = [None] * total_labels
+train_labels = [None] * total_labels
 
-        image = np.asarray(image, dtype=np.float64)
-        image_45 = np.asarray(image_45, dtype=np.float64)
-        image_90 = np.asarray(image_90, dtype=np.float64)
-        train_images.append(image)
-        train_images.append(image_45)
-        train_images.append(image_90)
-        label = [0]
-        train_labels.append(label)
-        train_labels.append(label)
-        type1 += 3
-        counter1 += 1
-    elif random_label == 2:
-        path = train_path2.pop(len(train_path2) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image_45 = image.rotate(45)
-        image_blur = image.filter(ImageFilter.GaussianBlur(1))
-        image_blur2 = image.filter(ImageFilter.GaussianBlur(2))
-        image_blur3 = image.filter(ImageFilter.GaussianBlur(3))
-        image_90 = image.rotate(90)
-        image_60 = image.rotate(60)
-        image_15 = image.rotate(15)
-        image_80 = image.rotate(80)
-        image_75 = image.rotate(75)
-        image_25 = image.rotate(25)
-        image = np.asarray(image, dtype=np.float64)
-        image_45 = np.asarray(image_45, dtype=np.float64)
-        image_90 = np.asarray(image_90, dtype=np.float64)
-        image_60 = np.asarray(image_60, dtype=np.float64)
-        image_15 = np.asarray(image_15, dtype=np.float64)
-        image_80 = np.asarray(image_90, dtype=np.float64)
-        image_75 = np.asarray(image_60, dtype=np.float64)
-        image_25 = np.asarray(image_15, dtype=np.float64)
-        image_blur = np.asarray(image_blur, dtype=np.float64)
-        image_blur2 = np.asarray(image_blur2, dtype=np.float64)
-        image_blur3 = np.asarray(image_blur3, dtype=np.float64)
-        train_images.append(image)
-        train_images.append(image_45)
-        train_images.append(image_90)
-        train_images.append(image_60)
-        train_images.append(image_15)
-        train_images.append(image_80)
-        train_images.append(image_75)
-        train_images.append(image_25)
-        train_images.append(image_blur)
-        train_images.append(image_blur2)
-        train_images.append(image_blur3)
-        label = [1]
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        type2 += 11
-        counter2 += 1
+paths = train_path1 + train_path2 + train_path3 + train_path4
 
-    elif random_label == 3:
-        path = train_path3.pop(len(train_path3) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        
-        if counter3 < int(label_counts[2]*.1):
-            image_80 = image.rotate(80)
-            image_80 = np.asarray(image_80, dtype=np.float64)
-            train_images.append(image_80)
-            label = [0]
-            train_labels.append(label)
-            type3 += 1
-            counter3 += 1
+train_labels = [0] * len(train_path1) + [1] * len(train_path2) + [2] * len(train_path3) + [3] * len(train_path4)
 
-        image = np.asarray(image, dtype=np.float64)
-        train_images.append(image)
-        
-        type3 += 1
-        counter3 += 1
-    elif random_label == 4:
-        path = train_path4.pop(len(train_path4) - 1) # get the path at the end of the list
-        image = Image.open(path)
-        image_45 = image.rotate(45)
-        image_blur = image.filter(ImageFilter.GaussianBlur(1))
-        image_blur2 = image.filter(ImageFilter.GaussianBlur(2))
-        image_blur3 = image.filter(ImageFilter.GaussianBlur(3))
-        image_blur4 = image.filter(ImageFilter.GaussianBlur(4))
-        image_90 = image.rotate(90)
-        image_60 = image.rotate(60)
-        image_15 = image.rotate(15)
-        image_80 = image.rotate(80)
-        image_75 = image.rotate(75)
-        image_25 = image.rotate(25)
-        image = np.asarray(image, dtype=np.float64)
-        image_45 = np.asarray(image_45, dtype=np.float64)
-        image_90 = np.asarray(image_90, dtype=np.float64)
-        image_60 = np.asarray(image_60, dtype=np.float64)
-        image_15 = np.asarray(image_15, dtype=np.float64)
-        image_80 = np.asarray(image_90, dtype=np.float64)
-        image_75 = np.asarray(image_60, dtype=np.float64)
-        image_25 = np.asarray(image_15, dtype=np.float64)
-        image_blur = np.asarray(image_blur, dtype=np.float64)
-        image_blur2 = np.asarray(image_blur2, dtype=np.float64)
-        image_blur3 = np.asarray(image_blur3, dtype=np.float64)
-        image_blur4 = np.asarray(image_blur4, dtype=np.float64)
-        train_images.append(image)
-        train_images.append(image_45)
-        train_images.append(image_90)
-        train_images.append(image_60)
-        train_images.append(image_15)
-        train_images.append(image_80)
-        train_images.append(image_75)
-        train_images.append(image_25)
-        train_images.append(image_blur)
-        train_images.append(image_blur2)
-        train_images.append(image_blur3)
-        train_images.append(image_blur4)
-        label = [3]
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        train_labels.append(label)
-        type4 += 12
-        counter4 += 1
-    else:
-        print("Issue...")
-
-print("HELOOOO", type3)
+for i, path in enumerate(paths):
+    train_images[i] = np.asarray(Image.open(path.split("anu/")[1]), dtype=np.float64)
 
 train_images = np.asarray(train_images)
 train_labels = np.asarray(train_labels)
-'''
-#train_images = np.reshape(train_images.shape[0], train_images.shape[1], train_images.shape[2], 1)
-train_images_new = np.ones((train_images.shape[0], train_images.shape[1], train_images.shape[2], 1))
 
-for i in range(len(train_images)):
-    image = train_images[i]
-    #print(image.shape,  train_images.shape)
-    image = image.reshape(image.shape[0],image.shape[1],1) # <-- this makes it a rank-3 image
-    #print(image.shape)
-    train_images_new[i] = image
-
-print(train_images_new.shape)
-train_images = train_images_new
-'''
-print("New image count:", train_images.shape[0])
-
-total = type1 + type2 + type3 + type4
-
-print("Out of", total, "images, there are", type1, "in class 1,", type2, \
-      "in class 2,", type3, "in class 3,", "and", type4, "in class 4")
-
-
+test_size = 1 - cfg["train_size"] - cfg["secret_size"]
 
 secret_images, test_images, secret_labels, test_labels = train_test_split(train_images,
                                                                        train_labels,
-                                                                       test_size = 0.30,
-                                                                       train_size = 0.05, # this is actually the secret category
-                                                                       random_state = seed_value)
+                                                                       test_size = test_size,
+                                                                       train_size = cfg["secret_size"], # this is actually the secret category
+                                                                       random_state = cfg["seed_value"])
 
 train_images, temp_images, train_labels, temp_labels = train_test_split(train_images,
-        train_labels, train_size = 0.65, random_state = seed_value)
+        train_labels, train_size = cfg["train_size"], random_state = cfg["seed_value"])
 
-print("----- TRAIN/TEST SPLIT: 65% training, 30% testing, 5% secret -----")
-
-train_1_count, train_2_count, train_3_count, train_4_count = 0, 0, 0, 0
-
-for i in range(len(train_labels)):
-    if train_labels[i,0] == 0:
-        train_1_count += 1
-    elif train_labels[i,0] == 1:
-        train_2_count += 1
-    elif train_labels[i,0] == 2:
-        train_3_count += 1
-    elif train_labels[i,0] == 3:
-        train_4_count += 1
-        
-total = train_1_count + train_2_count + train_3_count + train_4_count
-
-print("Out of", total, "training images, there are", train_1_count, "in class 1,", train_2_count, \
-      "in class 2,", train_3_count, "in class 3,", "and", train_4_count, "in class 4")
-
-test_1_count, test_2_count, test_3_count, test_4_count = 0, 0, 0, 0
-
-for i in range(len(test_labels)):
-    if test_labels[i,0] == 0:
-        test_1_count += 1
-    elif test_labels[i,0] == 1:
-        test_2_count += 1
-    elif test_labels[i,0] == 2:
-        test_3_count += 1
-    elif test_labels[i,0] == 3:
-        test_4_count += 1
-
-total = test_1_count + test_2_count + test_3_count + test_4_count
-
-print("Out of", total, "testing images, there are", test_1_count, "in class 1,", test_2_count, \
-      "in class 2,", test_3_count, "in class 3,", "and", test_4_count, "in class 4\n\n")
+print("----- TRAIN/TEST SPLIT: " + str(cfg["train_size"]*100) + "% training, " + str(test_size*100) + "% validation, " + str(cfg["secret_size"]*100) + "%  test -----")
 
 train_images = train_images / 255
 test_images = test_images / 255
 
 print(train_images[1,:,:].shape)
 
-''' # comment this out if you want to use the keras tuner
+model = Sequential() 
 
-def model_builder(hp):
-    model = keras.Sequential()
-    model.add(layers.Flatten(input_shape = (84, 84, 3)))
+model.add(Conv2D(cfg["num_filters"], (cfg["square_filter_size"], cfg["square_filter_size"]), activation='relu', input_shape=(84, 84, 3)))
+model.add(MaxPooling2D((cfg["pool_size"], cfg["pool_size"])))
 
-    hp_units = hp.Int('units', min_value = 16, max_value = 512, step = 16)
-    model.add(keras.layers.Dense(units = hp_units, activation = 'relu'))
-    model.add(keras.layers.Dense(4))
+for _ in range(cfg["conv_layers"]-1):
+    model.add(Conv2D(cfg["num_filters"], (cfg["square_filter_size"], cfg["square_filter_size"]), activation='relu'))
+    model.add(MaxPooling2D((cfg["pool_size"], cfg["pool_size"])))
 
-    hp_learning_rate = hp.Choice('learning_rate', values = [1e-2, 1e-3, 1e-4])
+model.add(Flatten()) 
 
-    model.compile(optimizer = optimizers.Adam(learning_rate = hp_learning_rate),
-            loss = keras.losses.SparseCategoricalCrossentropy(from_logits = True),
-            metrics = ['accuracy'])
-
-    return model
-
-
-tuner= kt.Hyperband(model_builder,
-        objective = 'val_accuracy',
-        max_epochs = 10,
-        factor = 3,
-        directory = 'my_dir',
-        project_name = 'intro_to_kt',
-        overwrite = True)
-
-class ClearTrainingOutput(tf.keras.callbacks.Callback):
-    def on_train_end(*args, **kwargs):
-        IPython.display.clear_output(wait = True)
-
-tuner.search(train_images,train_labels, epochs = 10, validation_data = (test_images, test_labels), callbacks = [ClearTrainingOutput()])
-
-# Get the optimal hyperparameters
-best_hps = tuner.get_best_hyperparameters(num_trials = 1)[0]
-
-print("The hyperparameter search is complete. The optimal number of units in the first densely-connected layer is {best_hps.get('units')} and the optimal learning rate for the optimizer is {best_hps.get('learning_rate')}.")        
-
-model = tuner.hypermodel.build(best_hps)
-model.fit(train_images, train_labels, epochs = 10, validation_data = (test_images, test_labels))
-
-'''
-
-model = models.Sequential() 
-
-model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(84, 84, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(16, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Conv2D(32, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-
-model.add(layers.Flatten()) 
-
-model.add(layers.Dense(168, activation='relu'))
-model.add(layers.Dense(4)) # final Dense layer has 4 neurons representing the 4 classes
+model.add(Dense(cfg["dense_layer_dim"], activation='relu'))
+model.add(Dense(4)) # final Dense layer has 4 neurons representing the 4 classes
 
 print(model.summary())
 
@@ -356,28 +110,28 @@ model.compile(optimizer='Adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=20, #batch_size=32,
+history = model.fit(train_images, train_labels, epochs=cfg["epochs"], #batch_size=32,
                     validation_data=(test_images, test_labels))
 
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
+val_loss, val_acc = model.evaluate(test_images, test_labels, verbose=1)
 
 prediction = model.predict(secret_images, verbose=1)
-#print(prediction)
+
+model.save("checkpoint_dir/latest.h5")
 
 predicted_label = []
 incorrect_labels = []
-class_names = [1, 2, 3, 4]
 
 for i in range(len(prediction)):
-#    predicted_label.append(class_names[np.argmax(prediction[i])])
-    if int(class_names[int(secret_labels[i][0])]) != int(class_names[np.argmax(prediction[i])]):
-       # print("The true label was", int(class_names[int(secret_labels[i][0])]), "and the predicted label was", int(class_names[np.argmax(prediction[i])]))
-        incorrect_labels.append(int(class_names[int(secret_labels[i][0])]))
-        predicted_label.append(class_names[np.argmax(prediction[i])])
+    if secret_labels[i] != np.argmax(prediction[i]):
+        incorrect_labels.append(secret_labels[i])
+        predicted_label.append(np.argmax(prediction[i]))
 
 print("The number of incorrect labels is:", len(incorrect_labels))
 
 incorrect_1, incorrect_2, incorrect_3, incorrect_4 = 0, 0, 0, 0
+
+test_acc = 1 - len(incorrect_labels)/len(prediction)
 
 for i in range(len(incorrect_labels)):
     if incorrect_labels[i] == 1:
@@ -391,11 +145,14 @@ for i in range(len(incorrect_labels)):
 
 print("Out of", len(incorrect_labels), "incorrectly predicted labels,", incorrect_1, "were actually in class 1,", incorrect_2, "were actually in class 2,", incorrect_3, "were actually in class 3, and", incorrect_4, "were actually in class 4.")
 
-print("Class 1 accuracy:", int(100*(1-incorrect_1/len(incorrect_labels))), "%")
-print("Class 2 accuracy:", int(100*(1-incorrect_2/len(incorrect_labels))), "%")
-print("Class 3 accuracy:", int(100*(1-incorrect_3/len(incorrect_labels))), "%")
-print("Class 4 accuracy:", int(100*(1-incorrect_4/len(incorrect_labels))), "%")
+print("Test accuracy", int(test_acc*10000)/100, "%")
 
-# ''' # comment this quotes set out if you want to use the keras manually added layers
+c1_acc = 1-incorrect_1/len(incorrect_labels)
+c2_acc = 1-incorrect_2/len(incorrect_labels)
+c3_acc = 1-incorrect_3/len(incorrect_labels)
+c4_acc = 1-incorrect_4/len(incorrect_labels)
 
-
+print("Class 1 accuracy:", int(c1_acc*10000)/100, "%")
+print("Class 2 accuracy:", int(c2_acc*10000)/100, "%")
+print("Class 3 accuracy:", int(c3_acc*10000)/100, "%")
+print("Class 4 accuracy:", int(c4_acc*10000)/100, "%")
